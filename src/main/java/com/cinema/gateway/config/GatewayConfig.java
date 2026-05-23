0adb7edbf93df95996f2ff62f77cf00f9e50b54c
JaviCM4 <javic4m@gmail.com>
2026-05-23 11:26:51 -0600

    fix: Adding cinema route

--- src/main/java/com/cinema/gateway/config/GatewayConfig.java
+++ src/main/java/com/cinema/gateway/config/GatewayConfig.java
@@ -39,7 +39,6 @@ public class GatewayConfig {
                         .uri(moviesService))
                 .route("cinemas-private-routes", route -> route.path("/cinemas/**")
                         .filters(filter -> filter
-                                .stripPrefix(1)
                                 .filter(verifyJWTFilterFactory.apply(verifyConfig)))
                         .uri(cinemasService))
                 .route("tickets-private-routes", route -> route.path("/tickets/**")
